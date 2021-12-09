6a266aa7544e2c96688c15a5300ce6b3100c9879
ismail <ismailamjad98@yahoo.com>
2021-12-09 11:33:40 +0500

    4th Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,27 @@ use App\Mail\Sendmail;
 
 class UserController extends Controller
 {
+    
+    public function createToken($data)
+    {
+        try {
+            $key = config('constant.key');
+            $payload = array(
+                "iss" => "http://127.0.0.1:8000",
+                "aud" => "http://127.0.0.1:8000/api",
+                "iat" => time(),
+                "nbf" => 1357000000,
+                "id" => $data,
+                'token_type' => 'bearer',
+            );
+
+            $token = JWT::encode($payload, $key, 'HS256');
+            return $token;
+        } catch (Throwable $e) {
+            return response(['message' => $e->getMessage()]);
+        }
+    }
+
     /**
      * Registering a new user.
      */
@@ -26,10 +47,11 @@ class UserController extends Controller
         try {
             // Validate the user inputs
             $request->validated();
-
+            
             //create a link to varify email.      
             // $verification_token = (new createToken)->createToken($request->email);
-            // $url = "http://127.0.0.1:8000/api/emailVerify/" . $verification_token . '/' . $request->email;
+            $verification_token = $this->createToken($request->email);
+            $url = "http://127.0.0.1:8000/api/emailVerify/" . $verification_token . '/' . $request->email;
 
             if ($image = $request->file('profile_pic')) {
                 //make a path to store image
