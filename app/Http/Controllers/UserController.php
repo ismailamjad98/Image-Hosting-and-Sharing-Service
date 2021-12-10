bfd6f15a1276b5907f1c3d2879527696bf0a842a
ismail <ismailamjad98@yahoo.com>
2021-12-10 11:24:43 +0500

    fifth commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,7 +65,7 @@ class UserController extends Controller
                 'email' => $request->email,
                 'age' => $request->age,
                 'verification_token' => $url,
-                'profile_pic' => $profileImage,
+                // 'profile_pic' => $profileImage,
                 'password' => Hash::make($request->password),
             ]);
 
