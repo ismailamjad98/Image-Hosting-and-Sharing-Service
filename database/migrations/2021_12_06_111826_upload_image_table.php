865bd45b50ccd92774575b3a58853aed84ec5109
ismail <ismailamjad98@yahoo.com>
2021-12-14 10:16:11 +0500

    last commit

--- database/migrations/2021_12_06_111826_upload_image_table.php
+++ database/migrations/2021_12_06_111826_upload_image_table.php
@@ -15,10 +15,10 @@ class UploadImageTable extends Migration
     {
         Schema::create('upload_images', function (Blueprint $table) {
             $table->id();
-            $table->text('image')->nullable();
-            $table->text('link')->nullable();
+            $table->text('image');
+            $table->text('link');
             $table->integer('user_id')->nullable();
-            $table->string('status')->nullable();
+            $table->string('status')->default('hidden');
             $table->timestamps();
         });
     }
